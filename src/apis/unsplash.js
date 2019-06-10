f8610c973fe225cbf5cf799695670e93ca488778
emo88em <40817505+emo88em@users.noreply.github.com>
2019-06-09 21:57:31 -0500

    Update unsplash.js

--- src/apis/unsplash.js
+++ src/apis/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 292e76bd5fa8a0e86960b6f506636020c2ef7ce6423b2b663e874ded410a28bb'
+        Authorization: 'Client-ID '
     }
 });
